408af419def998f2d7364b5aabba61cef750480c
Antonioneto0306 <carneiroantonio3008@gmail.com>
2025-04-14 22:27:27 +0000

    desafiograph concluido

--- aula6/src/main.rs
+++ aula6/src/main.rs
@@ -1,32 +1,67 @@
-use petgraph::dot::{Dot, Config};
-use petgraph::graph::Graph;
-use std::fs::File;
-use std::io::Write;
+use std::collections::{HashMap, HashSet, VecDeque };
 
-fn main() -> std::io::Result<()> {
-    let mut graph = Graph::<&str, &str>::new();
-
-    //Adiciona Nós
-    let a = graph.add_node("A");
-    let b = graph.add_node("B");
-    let c = graph.add_node("C");
-    let d = graph.add_node("D");
-    let e = graph.add_node("E");
-    
-    //Arestas
-    graph.add_edge(a, e, "");
-    graph.add_edge(d, b, "");
-    graph.add_edge(c, a, "");
-    graph.add_edge(e, b, "");
-    graph.add_edge(c, d, "");
-    
-    //.dot
-    let dot_output = format!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
-
-    let mut file = File::create("graph.dot")?;
-    file.write_all(dot_output.as_bytes())?;
-
-    println!("Arquivo gravado com sucesso");
-    Ok(())
+struct Graph {
+    adjacency_lyst: HashMap<String, Vec<String>>,
 
 }
+
+impl Graph {
+    fn new() -> Self {
+        Graph{
+            adjacency_lyst: HashMap::new(),
+        }
+    }
+
+    fn add_vertex(&mut self, vertex: &str) {
+        self.adjacency_lyst.entry(vertex.to_string()).or_insert(Vec::new());
+    }
+
+    fn add_edge(&mut self, from: &str, to: &str) {
+        self.adjacency_lyst.entry(from.to_string()).or_insert(Vec::new()).push(to.to_string());
+        self.adjacency_lyst.entry(to.to_string()).or_insert(Vec::new()).push(from.to_string());
+    }
+
+    fn dfs(&self, start: &str) {
+        let mut visited = Hash::new();
+        println!("DFS");
+        self.dfs_recursive(start, &mut visited);
+    }
+
+    dn dfs_recursive(&self, vertex: &str, visited: &mut HashSet<String>) {
+        if visited.contains(vertex) {
+            return;
+        }
+        println!("{}", vertex);
+        visited.insert((vertex.to_string()));
+
+        if let Some(neighbors) = sel.adjacency_lyst.get(vertex) {
+            for neighbor in neighbors {
+                self.dfs_recursive(neighbor, visited);
+            }
+        }
+    }
+
+    fn bfs(&self start: &str) {
+        let mut visited = HashSet::new();
+        let mut queue = VecDeque::new();
+
+        queue.push_back(start.to_string());
+        visited.insert(start.to_string());
+
+        println!("BFS")
+
+        while let Some(current) = queue.pop_front() {
+            println!("{}", current);
+
+            if let Some(neighbors) = self.adjacency_lyst.get(&current) {
+                for neighbor in neighbors {
+                    if !visited.contains(neighbor) {
+                        visited.insert(neighbor.clone());
+                        queue.push_back(neighbor.clone());
+                    }
+                }
+            }
+        }
+    }
+}
+
